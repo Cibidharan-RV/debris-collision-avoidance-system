17dff5ecf857dd71eeb02e02f84e19fe6825a814
Cibidharan-RV <rvcibidharan@gmail.com>
2026-09-08 21:00:19 +0530

    refactor: implement broad and fine phase algorithm
    This update rewrites the conjunction analyzer fully to use a two-step algorithm.
    - Implements a 1-minute broad phase using numpy arrays higher speed.
    - Adds an automatic 'co-orbital' filter to ignore permanently docked modules.
    - Implements a 1-second fine phase that pinpoints the exact Time of Closest Approach-TCA.

--- src/conjunction_analyzer.py
+++ src/conjunction_analyzer.py
@@ -1,5 +1,6 @@
 from skyfield.api import load
 from datetime import timedelta
+import numpy as np
 from tle_manager import fetch_active_satellites
 
 def calculate_distance():
@@ -26,7 +27,12 @@ def calculate_distance():
 
     print(f"Distance between ISS and Tiangong: {distance_km:.2f} km")
 
-def find_conjunctions(target_id:int, threshold_km:float, search_window_mins:int = 90) -> list[dict]:
+def find_conjunctions(
+        target_id:int,
+        fine_threshold_km:float = 10.0,
+        search_window_mins:int = 90,
+        broad_threshold_km:float = 500
+    ) -> list[dict]:
     print("loading conjunctions...")
     satellites = fetch_active_satellites()
     by_id = {sat.model.satnum : sat for sat in satellites}
@@ -42,55 +48,49 @@ def find_conjunctions(target_id:int, threshold_km:float, search_window_mins:int 
     print(f"calculating positions over {search_window_mins} minutes for {target.name} ({target_id})")
 
     target_trajectory = target.at(times)
-    found_conjunctions = False
 
+    close_approaches = []
     for sat_id, sat in by_id.items():
         if sat_id == target_id:
             continue
-
+        
         diff = sat.at(times) - target_trajectory
         distances = diff.distance().km
 
-        close_approaches = []
-        in_conjunction = False
-        current_event = []
-        for i, dist in enumerate(distances):
-            if dist < threshold_km:
-                in_conjunction = True
-                current_event.append((i, dist))
-            else:
-                if in_conjunction and len(current_event) < 60: # to prevent permanently docked parts, like in ISS ans CSS
-                    best_idx, min_dist = min(current_event, key=lambda item: item[1])
+        if (np.min(distances) < broad_threshold_km):
+            if np.max(distances) < broad_threshold_km:
+                continue # co-orbotal objects
 
-                    close_approaches.append({
-                        "satellite_id": sat_id,
-                        "satellite_name": sat.name,
-                        "time": times[best_idx].utc_datetime(),
-                        "distance_km": min_dist
-                    })
-                    
-                    in_conjunction = False
-                    current_event = []
+            broad_mid_idx = np.argmin(distances)
+            broad_tca_time = time_steps[broad_mid_idx]
 
-        if in_conjunction and len(current_event) < 60:
-            best_idx, min_dist = min(current_event, key=lambda item: item[1])
+            # FINE PHASE START
+            fine_start = broad_tca_time - timedelta(seconds=60)
+            fine_time_steps = [fine_start + timedelta(seconds=i) for i in range(120)]
+            fine_times = ts.from_datetimes(fine_time_steps)
             
-            close_approaches.append({
-                "satellite_id": sat_id,
-                "satellite_name": sat.name,
-                "time": times[best_idx].utc_datetime(),
-                "distance_km": min_dist
-            })
+            fine_target = target.at(fine_times)
+            fine_diff = sat.at(fine_times) - fine_target
+            fine_distances = fine_diff.distance().km
+
+            fine_min_idx = np.argmin(fine_distances)
+            fine_tca_time = fine_time_steps[fine_min_idx]
+            fine_min_dist = fine_distances[fine_min_idx]
             
-            in_conjunction = False
-            current_event = []
-        if (len(close_approaches)):
-            found_conjunctions= True
+            if fine_min_dist <= fine_threshold_km:
+                close_approaches.append({
+                    "satellite_id" : sat_id,
+                    "satellite_name" : sat.name,
+                    "time" : fine_tca_time,
+                    "distance_km" : fine_min_dist
+                })
+
+    if len(close_approaches) > 0:
         for approach in close_approaches:
             print(f"Conjunction detected: {approach['satellite_name']} \t at {approach['time'].strftime('%Y-%m-%d %H:%M:%S')} UTC | {approach['distance_km']:.2f} km")
-    
-    if not found_conjunctions:
-        print(f"No conjuctions found for {target.name} ({target_id}) within {search_window_mins} minutes")
-    return close_approaches
+
+    else:
+        print("No conjunctions detected.")
+
 if __name__ == "__main__":
-    find_conjunctions(25544, 10.0, 1440)
+    find_conjunctions(25544, search_window_mins=1440)
